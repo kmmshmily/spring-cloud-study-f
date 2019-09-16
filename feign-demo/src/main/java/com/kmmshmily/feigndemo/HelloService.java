39b135570ad01ffd0c4bb33875d5aa30506d332c
kmmshmily <1160634082@qq.com>
2019-09-16 12:59:30 +0800

    feign demo--待完善

---
+++ feign-demo/src/main/java/com/kmmshmily/feigndemo/HelloService.java
@@ -0,0 +1,21 @@
+package com.kmmshmily.feigndemo;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @program: spring-cloud-study-f
+ * @description: HelloService
+ * @author: kmmshmily--cuiyong
+ * @date: 2019-09-16 12:52
+ **/
+
+@FeignClient(name = "github-client", url = "https://api.github.com")
+public interface HelloService {
+
+    @RequestMapping(value = "/search/repositories", method = RequestMethod.GET)
+    String searchRepo(@RequestParam("q") String queryStr);
+
+}   
